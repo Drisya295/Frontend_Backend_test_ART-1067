08953ae240cbb77eea29968586b41010def14f27
Drisya295 <shimsudrish@gmail.com>
2024-09-06 00:04:14 +0800

    Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+public class Employee
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string EmailAddress { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Gender { get; set; }
+    public DateTime StartDate { get; set; }
+    public Guid? CafeId { get; set; }
+    public Cafe Cafe { get; set; }
+}
+
