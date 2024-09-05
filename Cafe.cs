55069d889eb21842aa1ef8453ffb62b7d11ed152
Drisya295 <shimsudrish@gmail.com>
2024-09-06 00:03:37 +0800

    Cafe.cs

---
+++ Cafe.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using System;
+
+public class Cafe
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Logo { get; set; }
+    public string Location { get; set; }
+    public List<Employee> Employees { get; set; }
+}
