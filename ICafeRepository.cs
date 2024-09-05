f9d0a6c1b28cb49b0f7dee00ae30062bb992440f
Drisya295 <shimsudrish@gmail.com>
2024-09-06 00:04:34 +0800

    ICafeRespository.cs

---
+++ ICafeRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+public interface ICafeRepository
+{
+    Task<IEnumerable<Cafe>> GetCafesAsync(string location = null);
+    Task<Cafe> GetCafeByIdAsync(Guid id);
+    Task CreateCafeAsync(Cafe cafe);
+    Task UpdateCafeAsync(Cafe cafe);
+    Task DeleteCafeAsync(Guid id);
+}
+
